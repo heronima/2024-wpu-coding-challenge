1300bef4370977066e6f373fa182b643a374509a
rinigadjo <rinigadjo@gmail.com>
2024-01-23 23:15:02 +0800

    day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,17 @@
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result = result * x[i];
+//   }
+//   return result;
+// }
+// console.log(grow([1, 2, 3, 4]));
+
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
